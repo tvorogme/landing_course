fc74d910320ccd6af8b351ef53aef88225fc5f79
Andrey Tvorozhkov <tvorog@tvorog.me>
2020-03-19 10:31:29 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {a: props.a, from_props_a: 0};
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        return {from_props_a: props.a}
+    }
+
+    f = () => {
+
+        const newState = this.state.a + 1;
+        console.log(this.state.a, newState);
+        this.setState({a: newState})
+    };
+
+    render() {
+        return <div>
+            <h1>LOLKEK {this.state.a + this.state.from_props_a} {this.state.b} {this.state.color}</h1>
+            <button onClick={this.f}>Add One</button>
+        </div>
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {a: 1};
+    }
+
+    f = () => {
+        const newState = this.state.a + 1;
+        this.setState({a: newState});
+        console.log(newState);
+    };
+
+    render() {
+
+
+        return <div>
+            <h2>Привет!</h2>
+            <button onClick={this.f}>GLOBAL ADD</button>
+
+            {[...Array(10).keys()].map((x) => <Calculator key={x} a={this.state.a}/>)}
+
+
+        </div>
+    }
 }
 
 export default App;
